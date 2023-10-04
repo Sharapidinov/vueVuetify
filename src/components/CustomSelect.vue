15f12d293e49c69a32fe782a316d764d8b91f480
kamaqwerty <kamchybekovkama@gmail.com>
2023-10-04 16:04:09 +0600

    fix

--- src/components/CustomSelect.vue
+++ src/components/CustomSelect.vue
@@ -1,55 +1,44 @@
 <template>
     <div class="input__box">
-        <label v-if="config.prefix.show" :style="config.prefix.style">
-
-        </label>
-        <v-select
-            :items="selectData.map(item => item[config?.labelKey])"
-            :placeholder="config?.placeholder"
-            v-model="selectedValue"
-            :label="config.label "
-            :variant="config.typeInput"
-            :item-value="config.valueKey"
-            :item-text="config.labelKey"
-            @change="handleChange"
-        />
-
-
-
+      <label v-if="config.prefix.show" :style="config.prefix.style">{{ config.prefix.text }}</label>
+      <v-select
+  :items="selectData"
+  :placeholder="config?.placeholder"
+  v-model="selectedValue"
+  :label="config.label"
+  :variant="config.typeInput"
+  :item-title="config.keyNLS"
+  :item-value="config.key"
+  @change="handleChange"
+/>
+      <p>Выбранное значение: {{ selectedValue }}</p>
     </div>
-</template>
-
-<script>
-
-export default {
+  </template>
+  
+  <script>
+  export default {
     props: {
-        config: Object,
-        selectData: Array,
-        value: Object, // Принимаем объект в качестве prop
+      config: Object,
+      selectData: Array,
+      value: Object,
     },
     data() {
-        return {
-            selectedValue: null,
-            selectedSubgroup: null, // Добавляем data для хранения выбранного объекта
-        };
+      return {
+        selectedValue: null,
+      };
     },
     methods: {
-
-        handleChange(newValue) {
-            console.log(newValue)
-
-        },
-        handleSubgroupChange(newValue) {
-
-        },
+      handleChange() {
+        this.$emit("change", this.selectedValue);
+      },
     },
-};
-</script>
-
-<style scoped>
-.input__box{
+  };
+  </script>
+  
+  <style>
+  .input__box {
     width: 50%;
     margin: 0 auto;
-}
-
-</style>
+  }
+  </style>
+  
